307854d7c1599655af9fd2a18a2da43be78de435
verenx <verenx@gmail.com>
2024-06-24 10:58:00 +0200

    updated package.json

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import "./App.css";
 export default function App() {
    return ( 
    <div className="App">
+   <div className="container">
       <WeatherSearch />
       <WeatherInfo />
     <footer>This project was coded by <a href="https://www.linkedin.com/in/verena-al-khazraji-a73bb370/" target="_blank" rel="noreferrer">Verena Al-Khazraji</a> and is hosted on <a href="https://github.com/verenx/react-weather-app" target="_blank" rel="noreferrer">Github</a></footer>
+   </div>
    </div>
    );
 }
